8dfe08f2d5e8097ea35c0d21814b68846a829bba
afsinka <afsinka@gmail.com>
2016-10-15 23:04:19 +0300

    add grunt maven plugin eirslett

--- WebContent/app/scripts/controllers/about.js
+++ WebContent/app/scripts/controllers/about.js
@@ -7,13 +7,9 @@
  */
 angular.module('webContentApp').controller('AboutCtrl', [ '$scope', '$sce', function($scope, $sce) {
 
-	var popup = '<ul style="color: #000 !important;">';
-	popup += ' <li>formerly Af&#x15F;in</li>';
-	popup += ' <li>pronounced as <i>Afsheen</i></li>';
-	popup += ' <li>an old word from the Seljuk Empire</li>';
-	popup += ' <li>means armor</li>';
-	popup += ' <li>a common name for boys and girls in Turkey and Azerbaijan</li>';
-	popup += ' </ul>'
+	var popup = '';
+	popup += ' pronounced as af-shin';
+	popup += ' ';
 	$scope.htmlPopover = $sce.trustAsHtml(popup);
 
 } ]);
